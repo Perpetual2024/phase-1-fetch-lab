59ae8f56f0392e9098a43faa4926e95035d57417
Perpetual Akinyi <you@example.com>
2024-10-10 22:52:32 +0300

    fetch

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-function fetchBooks() {
+/*function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+       fetch('https://anapioficeandfire.com/api/books', renderBooks)
+       .then((response=> response.json())
+       
+
 }
 
 function renderBooks(books) {
@@ -9,9 +12,34 @@ function renderBooks(books) {
     const h2 = document.createElement('h2');
     h2.innerHTML = book.name;
     main.appendChild(h2);
+    
   });
+  
 }
 
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
+*/
+// index.js
+
+function fetchBooks() {
+  return fetch('https://anapioficeandfire.com/api/books')
+    .then(response => response.json())
+    .then(data => {
+      renderBooks(data);
+    });
+}
+
+function renderBooks(books) {
+  const mainElement = document.querySelector('main');
+  books.forEach(book => {
+    const bookElement = document.createElement('div');
+    bookElement.textContent = book.name; // Assuming the book object has a 'name' property
+    mainElement.appendChild(bookElement);
+  });
+}
+
+// Expose functions for testing
+window.fetchBooks = fetchBooks;
+window.renderBooks = renderBooks;
